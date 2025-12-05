588b481fa3c95495bd368754569a899aed0a94af
hihikomori <ghhhhhnika@gmail.com>
2025-12-05 12:33:06 +0300

    feat: base structure

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+
+struct p_t
+{
+  int x, y;
+};
+
+
+struct IDraw
+{
+  virtual p_t begin() const = 0;
+  virtual p_t next(p_t) const = 0;//чисто виртуальные методы
+  virtual ~IDraw();
+};
+
 
 int main(){
   
