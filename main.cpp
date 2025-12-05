2a90196d7bcf23c335f78e0eb6a358a6a16caa07
hihikomori <ghhhhhnika@gmail.com>
2025-12-05 12:44:29 +0300

    feat: create IDraw interface

--- main.cpp
+++ main.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 
-namespace topit
-{
-  struct p_t
-  {
+namespace topit {
+  struct p_t {
     int x, y;
-  }; 
+  };
   bool operator==(p_t, p_t);
   bool operator!=(p_t, p_t);
-  struct f_t(p_t aa, bb);
+  /// struct f_t(p_t aa, bb);
+  struct IDraw{
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;//чисто виртуальные методы
+    virtual ~IDraw() = default;
+  };
+
 } // namespace topit
-
-
-
-int main(){
+int main() {
   using topit::p_t;
-  p_t a(1, 0), b(1, 0);
-  std::cout << a == b << '\n';
+  p_t a{1, 0}, b{1, 0};
+  std::cout << (a == b) << '\n';
 }
 
-bool topit::operator==(p_t a, p_t b){
-  return a.x == b.x && a.y == b.y;
-}
+bool topit::operator==(p_t a, p_t b) { return a.x == b.x && a.y == b.y; }
 
-bool topit::operator!=(p_t a, p_t b){
-  return !(a == b);
-}
+bool topit::operator!=(p_t a, p_t b) { return !(a == b); }
