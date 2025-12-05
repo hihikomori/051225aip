ff1c1b1a26b83a0347cf8a4b2bd10d7836591c62
hihikomori <ghhhhhnika@gmail.com>
2025-12-05 13:19:10 +0300

    feat: points and frame declaration

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ namespace topit {
   };
   bool operator==(p_t, p_t);
   bool operator!=(p_t, p_t);
-  /// struct f_t(p_t aa, bb);
+  struct f_t(p_t aa, bb);
   struct IDraw{
     virtual p_t begin() const = 0;
     virtual p_t next(p_t) const = 0;//чисто виртуальные методы
@@ -21,21 +21,30 @@ namespace topit {
     explicit Dot(p_t dd);
     p_t d;
   };
-  
+  size_t points(const IDraw& d, p_t** pts, size_t& s);
+  f_t frame(const p_t* pts, size_t& s);
 
 } // namespace topit
 
 int main() {
   using topit::IDraw;
   using topit::Dot;
+  using topit::p_t;
+  using topit::f_t;
   IDraw* shps[3] = {};
   int err = 0;
+  p_t * pts = nullptr;
+  size_t s = 0;
   try{
     shps[0] = new Dot(0, 0);
     shps[1] = new Dot(5, 7);
     shps[2] = new Dot(-5, -2);
     //TODO:
     //[1] достать все точки из фигур
+    for(size_t i = 0; i < 3; ++i){
+      s += points(*(shps[i]), &pts, s);
+    }
+    f_t fr = frame(pts, s);
     //[2] расчитать ограниченный прямоугольник
     //[3] подготовить полотно для рисования
     //[4] нарисовать на полотне все точки
